63c84fd94ab4c25d59ae2e181f59ad2826ccc91f
lmazardo <luc@mazardo.com>
2011-03-04 13:18:16 +0100

    NEW: initial revision

---
+++ coding.el
@@ -0,0 +1,129 @@
+(setq tab-width 2)
+(setq indent-tabs-mode nil)
+
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+ 
+(setq js2-basic-offset 2)
+(setq js2-use-font-lock-faces t)
+
+   ;; ido-mode and imenu
+    (defun ido-goto-symbol (&optional symbol-list)
+      "Refresh imenu and jump to a place in the buffer using Ido."
+      (interactive)
+      (unless (featurep 'imenu)
+        (require 'imenu nil t))
+      (cond
+       ((not symbol-list)
+        (let ((ido-mode ido-mode)
+              (ido-enable-flex-matching
+               (if (boundp 'ido-enable-flex-matching)
+                   ido-enable-flex-matching t))
+              name-and-pos symbol-names position)
+          (unless ido-mode
+            (ido-mode 1)
+            (setq ido-enable-flex-matching t))
+          (while (progn
+                   (imenu--cleanup)
+                   (setq imenu--index-alist nil)
+                   (ido-goto-symbol (imenu--make-index-alist))
+                   (setq selected-symbol
+                         (ido-completing-read "Symbol? " symbol-names))
+                   (string= (car imenu--rescan-item) selected-symbol)))
+          (setq position (cdr (assoc selected-symbol name-and-pos)))
+          (cond
+           ((overlayp position)
+            (goto-char (overlay-start position)))
+           (t
+            (goto-char position)))))
+       ((listp symbol-list)
+        (dolist (symbol symbol-list)
+          (let (name position)
+            (cond
+             ((and (listp symbol) (imenu--subalist-p symbol))
+              (ido-goto-symbol symbol))
+             ((listp symbol)
+              (setq name (car symbol))
+              (setq position (cdr symbol)))
+             ((stringp symbol)
+              (setq name symbol)
+              (setq position
+                    (get-text-property 1 'org-imenu-marker symbol))))
+            (unless (or (null position) (null name)
+                        (string= (car imenu--rescan-item) name))
+              (add-to-list 'symbol-names name)
+              (add-to-list 'name-and-pos (cons name position))))))))
+;    (global-set-key "\C-ci" 'ido-goto-symbol) ; or any key you see fit
+
+;;does not works
+(defun doctest-launch
+  (doctest-mode)
+  (doctest-execute)
+  (python-mode))
+
+
+(add-hook 'python-mode-hook 
+	  '(lambda ()
+	     (local-set-key [C-prior] 'py-beginning-of-def-or-class)
+	     (local-set-key [C-next] 'py-end-of-def-or-class)
+	     (local-set-key [C-return] 'ido-goto-symbol)
+	     (local-set-key [C-return] 'doctest-launch)
+	     (local-set-key [f7]     'doctest-mode)
+	     (hs-minor-mode t)))
+
+(add-hook 'doctest-mode-hook 
+	  '(lambda ()
+	     (local-set-key [f7]     'python-mode)))
+
+(require 'pymacs)
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
+(defun toggle-hiding (column)
+  (interactive "P")
+  (if hs-minor-mode
+      (if (condition-case nil
+              (hs-toggle-hiding)
+            (error t)) 
+          (hs-show-all))
+    (toggle-selective-display column)))
+(defcustom py-hide-show-keywords '("class" "def" "elif" "else" "except"
+  "for" "if" "while" "finally" "try" "with")
+   "*Keywords used by hide-show"
+   :type '(repeat string)
+   :group 'python)
+
+(defcustom py-hide-show-hide-docstrings t
+  "*If doc strings shall be hidden"
+  :type 'boolean
+  :group 'python)
+
+;;(python-python-command-arg 
+;;(defcustom python-python-command-args '("-i")
+
+;; Add support for HideShow
+(add-to-list 'hs-special-modes-alist (list
+                'python-mode (concat (if py-hide-show-hide-docstrings "^\\s-*\"\"\"\\|" "") (mapconcat 'identity (mapcar #'(lambda (x) (concat "^\\s-*" x "\\>")) py-hide-show-keywords ) "\\|")) nil "#"
+                (lambda (arg)
+                 (py-goto-beyond-block)
+                 (skip-chars-backward " \t\n"))
+                nil))
+
+
+(global-set-key (kbd "C-+") 'toggle-hiding)
+(global-set-key (kbd "C-=") 'comment-or-uncomment-region)
+
+
+(autoload 'hideshowvis-enable "hideshowvis" "Highlight foldable regions")
+
+(autoload 'hideshowvis-minor-mode
+  "hideshowvis"
+  "Will indicate regions foldable with hideshow in the fringe."
+  'interactive)
+
+
+(dolist (hook (list 'emacs-lisp-mode-hook
+                    'c++-mode-hook 'python-mode-hook) )
+  (add-hook hook 'hideshowvis-enable))
+
+
+(message "coding.el is loaded")
---
+++ main.el
@@ -0,0 +1,79 @@
+;; remove {menu,tool}bar , display time using particular format
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq-default fill-column 72)
+(setq auto-fill-mode 1)
+(setq inhibit-startup-message t)
+(setq column-number-mode t)
+(setq line-number-mode t)
+(defface my-display-time
+  '((((type x w32 mac))
+     (:foreground "pink" :background "black" : :inherit bold))
+    (((type tty))
+     (:foreground "blue")))
+  "Face used to display the time in the mode line.")
+(setq display-time-string-forms
+      '((propertize (concat " " 24-hours ":" minutes " ")
+ 		    'face 'my-display-time)))
+
+(display-time)
+
+
+(which-func-mode)
+
+(require 'color-theme)
+(require 'icicles)
+
+
+(load-library "my-color-theme")
+(load-library "anything")
+(global-font-lock-mode 1)
+;;(color-theme-initialize)
+(color-theme-ryan)
+
+;; visual editing
+(transient-mark-mode t)
+(show-paren-mode t)
+(setq show-paren-style 'expression)
+(global-font-lock-mode t) ;; automatic colorization
+(blink-cursor-mode 0)
+
+;; ido mode configuration
+(ido-mode t)
+(setq ido-enable-regexp t)
+(setq ido-enable-flex-matching t)
+(setq ido-execute-command-cache nil)
+(ido-everywhere t)
+
+(when (require 'recentf)
+  (setq recentf-auto-cleanup 'never) ;; évite de vérifier les fichiers distants au démarrage
+  (recentf-mode 1))
+
+; elscreen is screen for emacs (the key is C-z)
+(require 'elscreen)
+
+;; globals keys and mouse config
+(mouse-wheel-mode t)
+(global-set-key [C-tab] 'other-window)
+(global-set-key [C-return] 'kill-this-buffer)
+(global-set-key [(control >)]    'bs-cycle-next)
+(global-set-key [(control <)]   'bs-cycle-previous)
+(global-set-key [f5]     'linum-mode)
+(global-set-key [f6]     'menu-bar-mode)
+(global-set-key [f7]     'anything)
+(global-set-key [f8]     'next-error)
+(global-set-key [f9]     'compile) ;;my-compile-or-tramp-compile)
+(define-key global-map "\eg"    'goto-line)
+
+
+(load-library "coding")
+
+(require 'yasnippet)
+(yas/initialize)
+(setq yas/root-directory '("~/etc/emacs/mysnippets"
+      "/usr/share/emacs/site-lisp/yasnippet/snippets/"))
+(mapc 'yas/load-directory yas/root-directory)
+
+
+(message "main is loaded")
