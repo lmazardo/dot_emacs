1aab15d22b03c8979ab02e1553ec6e39f6721891
lmazardo <luc@mazardo.com>
2011-03-07 09:55:19 +0100

    CHANGE: change elisp path

--- dot_emacs.el
+++ dot_emacs.el
@@ -1,7 +1,7 @@
-(defconst lmo-elisp-path
+(defconst home-elisp-path
   (expand-file-name "~/etc/emacs")
     "Personal path for emacs stuff")
 
-(add-to-list 'load-path lmo-elisp-path)
+(add-to-list 'load-path home-elisp-path)
 (load-library "main")
 (message "dot emacs is loaded")
