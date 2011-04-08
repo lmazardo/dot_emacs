a4ed7598cc1267e0a6cb021bb6ef769cd6a48d66
lmazardo <luc@mazardo.com>
2011-04-08 15:16:53 +0200

    CHANGE: rearrange langage stuff

--- coding.el
+++ coding.el
@@ -1,75 +1,16 @@
-(setq tab-width 2)
-(setq indent-tabs-mode nil)
+;; Add support for HideShow visual
+(autoload 'hideshowvis-enable "hideshowvis" "Highlight foldable regions")
+(autoload 'hideshowvis-minor-mode
+  "hideshowvis"
+  "Will indicate regions foldable with hideshow in the fringe."
+  'interactive)
 
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
- 
-(setq js2-basic-offset 2)
-(setq js2-use-font-lock-faces t)
-
-(add-hook 'js2-mode-hook 
-	  '(lambda ()
-	     (local-set-key (kbd "C-+") 'js2-mode-toggle-element)))
+(dolist (hook (list 'emacs-lisp-mode-hook
+                    'c++-mode-hook
+		    'js2-mode-hook))
+  (add-hook hook 'hideshowvis-enable))
 
 
-   ;; ido-mode and imenu
-    (defun ido-goto-symbol (&optional symbol-list)
-      "Refresh imenu and jump to a place in the buffer using Ido."
-      (interactive)
-      (unless (featurep 'imenu)
-        (require 'imenu nil t))
-      (cond
-       ((not symbol-list)
-        (let ((ido-mode ido-mode)
-              (ido-enable-flex-matching
-               (if (boundp 'ido-enable-flex-matching)
-                   ido-enable-flex-matching t))
-              name-and-pos symbol-names position)
-          (unless ido-mode
-            (ido-mode 1)
-            (setq ido-enable-flex-matching t))
-          (while (progn
-                   (imenu--cleanup)
-                   (setq imenu--index-alist nil)
-                   (ido-goto-symbol (imenu--make-index-alist))
-                   (setq selected-symbol
-                         (ido-completing-read "Symbol? " symbol-names))
-                   (string= (car imenu--rescan-item) selected-symbol)))
-          (setq position (cdr (assoc selected-symbol name-and-pos)))
-          (cond
-           ((overlayp position)
-            (goto-char (overlay-start position)))
-           (t
-            (goto-char position)))))
-       ((listp symbol-list)
-        (dolist (symbol symbol-list)
-          (let (name position)
-            (cond
-             ((and (listp symbol) (imenu--subalist-p symbol))
-              (ido-goto-symbol symbol))
-             ((listp symbol)
-              (setq name (car symbol))
-              (setq position (cdr symbol)))
-             ((stringp symbol)
-              (setq name symbol)
-              (setq position
-                    (get-text-property 1 'org-imenu-marker symbol))))
-            (unless (or (null position) (null name)
-                        (string= (car imenu--rescan-item) name))
-              (add-to-list 'symbol-names name)
-              (add-to-list 'name-and-pos (cons name position))))))))
-
-(add-hook 'python-mode-hook 
-	  '(lambda ()
-	     (local-set-key [C-prior] 'py-beginning-of-def-or-class)
-	     (local-set-key [C-next] 'py-end-of-def-or-class)
-	     (local-set-key [C-return] 'ido-goto-symbol)
-	     (local-set-key (kbd "C-'") 'hippie-expand)
-	     (hs-minor-mode t)))
-(require 'pymacs)
-(pymacs-load "ropemacs" "rope-")
-
-(setq ropemacs-enable-autoimport t)
 (defun toggle-hiding (column)
   (interactive "P")
   (if hs-minor-mode
@@ -78,6 +19,61 @@
             (error t)) 
           (hs-show-all))
     (toggle-selective-display column)))
+
+
+(setq tab-width 2)
+(setq indent-tabs-mode nil)
+(global-set-key (kbd "C-+") 'toggle-hiding)
+(global-set-key (kbd "C-=") 'comment-or-uncomment-region)
+
+;; ido-mode and imenu
+(defun ido-goto-symbol (&optional symbol-list)
+  "Refresh imenu and jump to a place in the buffer using Ido."
+  (interactive)
+  (unless (featurep 'imenu)
+    (require 'imenu nil t))
+  (cond
+   ((not symbol-list)
+    (let ((ido-mode ido-mode)
+	  (ido-enable-flex-matching
+	   (if (boundp 'ido-enable-flex-matching)
+	       ido-enable-flex-matching t))
+	  name-and-pos symbol-names position)
+      (unless ido-mode
+	(ido-mode 1)
+	(setq ido-enable-flex-matching t))
+      (while (progn
+	       (imenu--cleanup)
+	       (setq imenu--index-alist nil)
+	       (ido-goto-symbol (imenu--make-index-alist))
+	       (setq selected-symbol
+		     (ido-completing-read "Symbol? " symbol-names))
+	       (string= (car imenu--rescan-item) selected-symbol)))
+      (setq position (cdr (assoc selected-symbol name-and-pos)))
+      (cond
+       ((overlayp position)
+	(goto-char (overlay-start position)))
+       (t
+	(goto-char position)))))
+   ((listp symbol-list)
+    (dolist (symbol symbol-list)
+      (let (name position)
+	(cond
+	 ((and (listp symbol) (imenu--subalist-p symbol))
+	  (ido-goto-symbol symbol))
+	 ((listp symbol)
+	  (setq name (car symbol))
+	  (setq position (cdr symbol)))
+	 ((stringp symbol)
+	  (setq name symbol)
+	  (setq position
+		(get-text-property 1 'org-imenu-marker symbol))))
+	(unless (or (null position) (null name)
+		    (string= (car imenu--rescan-item) name))
+	  (add-to-list 'symbol-names name)
+	  (add-to-list 'name-and-pos (cons name position))))))))
+
+;; Python setup
 (defcustom py-hide-show-keywords '("class" "def" "elif" "else" "except"
   "for" "if" "while" "finally" "try" "with")
    "*Keywords used by hide-show"
@@ -89,10 +85,6 @@
   :type 'boolean
   :group 'python)
 
-;;(python-python-command-arg 
-;;(defcustom python-python-command-args '("-i")
-
-;; Add support for HideShow
 (add-to-list 'hs-special-modes-alist (list
                 'python-mode (concat (if py-hide-show-hide-docstrings "^\\s-*\"\"\"\\|" "") (mapconcat 'identity (mapcar #'(lambda (x) (concat "^\\s-*" x "\\>")) py-hide-show-keywords ) "\\|")) nil "#"
                 (lambda (arg)
@@ -100,21 +92,47 @@
                  (skip-chars-backward " \t\n"))
                 nil))
 
+(defun lookup-pydoc () ;; was hohe2-lookup-pydoc
+  (interactive)
+  (let ((curpoint (point)) (prepoint) (postpoint) (cmd))
+    (save-excursion
+      (beginning-of-line)
+      (setq prepoint (buffer-substring (point) curpoint)))
+    (save-excursion
+      (end-of-line)
+      (setq postpoint (buffer-substring (point) curpoint)))
+    (if (string-match "[_a-z][_\\.0-9a-z]*$" prepoint)
+        (setq cmd (substring prepoint (match-beginning 0) (match-end 0))))
+    (if (string-match "^[_0-9a-z]*" postpoint)
+        (setq cmd (concat cmd (substring postpoint (match-beginning 0) (match-end 0)))))
+    (if (string= cmd "") nil
+      (let ((max-mini-window-height 0))
+        (shell-command (concat "pydoc " cmd))))))
 
-(global-set-key (kbd "C-+") 'toggle-hiding)
-(global-set-key (kbd "C-=") 'comment-or-uncomment-region)
+(require 'pymacs)
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
 
-(autoload 'hideshowvis-enable "hideshowvis" "Highlight foldable regions")
+(add-hook 'python-mode-hook 
+	  '(lambda ()
+	     (local-set-key [C-prior] 'py-beginning-of-def-or-class)
+	     (local-set-key [C-next] 'py-end-of-def-or-class)
+	     (local-set-key [C-return] 'ido-goto-symbol)
+	     (local-set-key (kbd "C-'") 'hippie-expand)
+	     (local-set-key (kbd "C-h f") 'lookup-pydoc)
+	     (hs-minor-mode t)
+	     (hideshowvis-enable)))
 
-(autoload 'hideshowvis-minor-mode
-  "hideshowvis"
-  "Will indicate regions foldable with hideshow in the fringe."
-  'interactive)
+;; Javascript mode
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+ 
+(setq js2-basic-offset 2)
+(setq js2-use-font-lock-faces t)
 
-
-(dolist (hook (list 'emacs-lisp-mode-hook
-                    'c++-mode-hook 'python-mode-hook 'js2-mode-hook) )
-  (add-hook hook 'hideshowvis-enable))
+(add-hook 'js2-mode-hook 
+	  '(lambda ()
+	     (local-set-key (kbd "C-+") 'js2-mode-toggle-element)))
 
 
 (message "coding.el is loaded")
